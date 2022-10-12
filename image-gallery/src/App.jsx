f2d1dfc8ab8b9c105003f78cdc642996879130b6
Muhammad Daud <86343612+MDaud32@users.noreply.github.com>
2022-10-12 20:23:09 +0500

    adding images and making it responsive

--- image-gallery/src/App.jsx
+++ image-gallery/src/App.jsx
@@ -1,4 +1,11 @@
 import { AiOutlineSearch } from 'react-icons/ai';
+import { BsBookmarkCheck } from 'react-icons/bs';
+import asset from './assets/image1.jpg';
+import image2 from './assets/image2.jpg';
+import image3 from './assets/image3.jpg';
+import image4 from './assets/image4.jpg';
+import image5 from './assets/image5.jpg';
+import image6 from './assets/image6.jpg';
 function App() {
   return (
     <div className="flex items-center justify-center bg-cyan-50 min-h-screen">
@@ -38,6 +45,93 @@ function App() {
           <button className="border-2 flex flex-col md:flex-row rounded-lg bg-black hover:bg-white text-white px-16 md:px-6 py-2 hover:text-black">
             Search
           </button>
+        </div>
+
+        <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4">
+          <div className="relative group">
+            <img src={asset} alt="first image" className="w-72" />
+            <div className="absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40 w-72 lg:w-[17.2rem] md:w-[11.1rem]">
+              <div className="flex justify-between w-full">
+                <div className="font-normal">
+                  <p className="text-sm">Abstact Painting</p>
+                  <p className="text-xs">245 Likes - 35 Shares</p>
+                </div>
+                <div className="flex items-center">
+                  <BsBookmarkCheck />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="relative group">
+            <img src={image2} alt="first image" className="w-72" />
+            <div className="absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40 w-72 lg:w-[17.2rem] md:w-[11.1rem]">
+              <div className="flex justify-between w-full">
+                <div className="font-normal">
+                  <p className="text-sm">Abstact Painting</p>
+                  <p className="text-xs">245 Likes - 35 Shares</p>
+                </div>
+                <div className="flex items-center">
+                  <BsBookmarkCheck />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="relative group">
+            <img src={image3} alt="first image" className="w-72" />
+            <div className="absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40 w-72 lg:w-[17.2rem] md:w-[11.1rem]">
+              <div className="flex justify-between w-full">
+                <div className="font-normal">
+                  <p className="text-sm">Abstact Painting</p>
+                  <p className="text-xs">245 Likes - 35 Shares</p>
+                </div>
+                <div className="flex items-center">
+                  <BsBookmarkCheck />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="relative group">
+            <img src={image4} alt="first image" className="w-72" />
+            <div className="absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40 w-72 lg:w-[17.2rem] md:w-[11.1rem]">
+              <div className="flex justify-between w-full">
+                <div className="font-normal">
+                  <p className="text-sm">Abstact Painting</p>
+                  <p className="text-xs">245 Likes - 35 Shares</p>
+                </div>
+                <div className="flex items-center">
+                  <BsBookmarkCheck />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="relative group">
+            <img src={image5} alt="first image" className="w-72" />
+            <div className="absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40 w-72 lg:w-[17.2rem] md:w-[11.1rem]">
+              <div className="flex justify-between w-full">
+                <div className="font-normal">
+                  <p className="text-sm">Abstact Painting</p>
+                  <p className="text-xs">245 Likes - 35 Shares</p>
+                </div>
+                <div className="flex items-center">
+                  <BsBookmarkCheck />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="relative group">
+            <img src={image6} alt="first image" className="w-72" />
+            <div className="absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40 w-72 lg:w-[17.2rem] md:w-[11.1rem]">
+              <div className="flex justify-between w-full">
+                <div className="font-normal">
+                  <p className="text-sm">Abstact Painting</p>
+                  <p className="text-xs">245 Likes - 35 Shares</p>
+                </div>
+                <div className="flex items-center">
+                  <BsBookmarkCheck />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
